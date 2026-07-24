2be2b50ed0caf0f786375edbdec262121a194da8
swarup_js <swarupsahu05@gmail.com>
2026-07-24 10:12:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log(Math.max(4, 6, 3 , 8 ));
+// let min = 5;
+// let max = 10;
+// console.log (Math.floor(Math.random()* (max - min + 1) + min));
+
+let myArr = [1, 2, 3, 4];
+
+myArr.push(5);
+
+console.log(myArr);
