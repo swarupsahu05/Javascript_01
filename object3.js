84c779e4d57d4e916b41ecc2cf65f0c03624f059
swarup_js <swarupsahu05@gmail.com>
2026-07-31 00:38:04 +0530

    Added object3.js

---
+++ object3.js
@@ -0,0 +1,9 @@
+const course = {
+    courseName: "JavaScript",
+    peice : "999",
+    courseInstructor: "Swarup Sahu",
+
+}
+
+const {courseInstructor: instructor} = course;
+console.log(instructor);
