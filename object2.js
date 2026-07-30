5fc9d8ea78af9267878389dc214baae64f0c8f97
swarup_js <swarupsahu05@gmail.com>
2026-07-31 00:05:54 +0530

    Added object2.js

---
+++ object2.js
@@ -0,0 +1,32 @@
+//const pubguser = new Object();
+
+ const pubguser = {}
+
+pubguser.id = 1344234224;
+pubguser.name = "swarup";
+pubguser.islogin = true;
+
+//console.log(pubguser);
+
+// const reguleruser = {
+//     fullname: {
+//         userfullname:{
+//             firstname: "swarup",
+//             lastname: "sahu"
+//         }
+//     }
+// }
+//console.log(reguleruser.fullname.userfullname.lastname);
+
+
+// const obj1 = {1: "swarup", 2: "sahu" }
+// const obj2 = {3: "Rahul", 4: "dolai" }
+
+// const obj3 = Object.assign({} , obj1, obj2);
+// console.log(obj3);
+//
+
+
+console.log(pubguser);
+
+console.log(Object.keys(pubguser));
