263d0e3b4092855bc9867499a12ef89694266c0a
swarup_js <swarupsahu05@gmail.com>
2026-08-14 20:07:08 +0530

    Add loops.js

---
+++ loops.js
@@ -0,0 +1,71 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i ;
+
+//     if (element == 5) {
+//         console.log("best time to break")
+//     }
+//     console.log('element:', i);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop ${j}`);
+//     }
+// }
+
+// let Array = ["swarup", "rahul", "sachin", "rohit", "virat"];
+
+// console.log(Array.length);
+
+// for (let i = 0; i < Array.length; i++) {
+//     const element = Array[i];
+//     console.log('element:', element);
+// }
+
+
+// Maps 
+
+// const map = new Map();
+
+// map.set('name', "swarup");
+// map.set('age', "21");
+// map.set('city', "west bengal");
+// map.set('age', "21");
+// // console.log(map);
+
+// for (const [key, value] of map){
+//     console.log(key,':-', value);
+// }
+
+
+// with object 
+
+// const myobject = {
+//     name: "swarup",
+//     age: 21,
+//     city: "west bengal"
+// }
+// for (const key in myobject) {
+//     console.log(key, ":-", myobject[key]);
+// }
+
+
+//for in loop with array
+// const programing  = ["name", "age", "city", "state", "country", "pincode"];
+
+// for ( const key in  programing) {
+//     // console.log(key);
+//     console.log(programing[key]);
+// }
+
+
+const coding = ["html", "css", "javascript", "react", "nodejs", "expressjs"];
+// coding.forEach((element) => {
+//    console.log (element)
+// })
+
+coding.forEach((item) => {
+    console.log(item)
+})
